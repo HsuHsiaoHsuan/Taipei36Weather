3722fe86b1ffbc18e504fa9ad4d6a1e077fb3a21
許孝萱 <hsuhsiaohsuan@xuxiaoxuans-MacBook-Pro.local>
2020-02-07 15:46:38 +0800

    init

---
+++ app/src/main/java/idv/hsu/taipei36weather/MainActivity.kt
@@ -0,0 +1,12 @@
+package idv.hsu.taipei36weather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
