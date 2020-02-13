fb15a1327bd183ba9f547d7149e213b5679a174a
Freeman <hsuhsiaohsuan+github@gmail.com>
2020-02-13 15:36:44 +0800

    detail 頁面改用 data binding

--- app/src/main/java/idv/hsu/taipei36weather/ui/detail/DetailActivity.kt
+++ app/src/main/java/idv/hsu/taipei36weather/ui/detail/DetailActivity.kt
@@ -4,16 +4,19 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import idv.hsu.taipei36weather.R
+import idv.hsu.taipei36weather.databinding.ActivityDetailBinding
 import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
-
-        val data = intent.getStringExtra("DATA")
-        tv_content.text = data
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_detail)
+        binding.minT = intent.getStringExtra("DATA")
     }
 
     companion object {
